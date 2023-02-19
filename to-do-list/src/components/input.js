59f8b198f97a5b11e48e02ded31c3c902d9748b7
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-19 14:40:54 +0530

    basic skelton

---
+++ to-do-list/src/components/input.js
@@ -0,0 +1,11 @@
+const Inputs = () => {
+    return (
+        <div className="to-do">
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+
+    )
+}
+
+export default Inputs
